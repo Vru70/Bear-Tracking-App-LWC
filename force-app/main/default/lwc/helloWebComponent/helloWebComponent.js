646bdf8de603f8fbfdbd42153cb50345f6798bc0
Vrushabh Uprikar <vrushabhuprikar02@gmail.com>
2021-04-26 19:52:48 +0530

    1st task

--- force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
+++ force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -1,3 +1,23 @@
+/**
+ * @author            : Vrushabh Uprikar
+ * @last modified on  : 04-26-2021
+ * @last modified by  : Vrushabh Uprikar
+ * Modifications Log 
+ * Ver   Date         Author             Modification
+ * 1.0   04-26-2021   Vrushabh Uprikar   Initial Version
+**/
 import { LightningElement } from 'lwc';
 
-export default class HelloWebComponent extends LightningElement {}
+export default class HelloWebComponent extends LightningElement
+{
+    greeting = 'Trailblazer';
+
+    handleGreetingChange(event) {
+        this.greeting = event.target.value;
+    }
+
+    currentDate = new Date().toDateString();
+    get capitalizedGreeting() {
+        return `Hello ${this.greeting.toUpperCase()}!`;
+    }
+}
